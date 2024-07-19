aaf339de4346671a0173c1c96e162fa5c6d9099d
Markuss Abolins <markuss.abolins@vasscompany.com>
2024-07-19 09:22:16 +0300

    TM-24: removed duplicate endpoint

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -11,11 +11,6 @@ router.get('/task/:id', (req, res) =>{
     return res.send("Would get task with id: " + _id);
 })
 
-router.get('/task/:id', (req, res) =>{
-    let _id = req.params.id
-    return res.send("Would get task with id: " + _id);
-})
-
 router.delete('removeTask/:id', (req, res) =>{
     let _id = req.params.id
     return res.send("Would delete task with id: " + _id);
