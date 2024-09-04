d16a069991b24907d38b12d15f52284b27a14451
Markuss Abolins <markuss.abolins@vasscompany.com>
2024-09-04 16:50:24 +0300

    TM-21: cors issue fix

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@ import express from 'express';
 import { json } from 'body-parser';
 import { taskRouter } from './routes/taskRoutes';
 import connectDB from "../config/db";
-import Cors from 'cors';
+import cors from 'cors';
 
 
 
 const app = express();
 const port = 3001;
+app.use(cors({origin:'http://localhost:4200'}));
 app.use(json());
 app.use(taskRouter);
-app.use(Cors({origin: 'http://localhost:4200'}));
 
 
 
